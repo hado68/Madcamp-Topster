d2ac05b643f38136cd11fe37a503abadc92011c6
Dohyun Ha <hdh6748@gmail.com>
2024-07-22 21:01:41 +0900

    안녕

---
+++ src/pages/turntable.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+import TurnTableComponent from '../components/TurnTable';
+
+export default function TurnTable() {
+  return (
+    <>
+      <Head>
+        <title>Album Shelf</title>
+        <meta name="description" content="TurnTable Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', margin: 0, backgroundColor: '#f0f0f0' }}>
+        <TurnTableComponent />
+      </main>
+    </>
+  );
+}
