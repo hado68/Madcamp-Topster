6ca2b00a22dc3df2f01d46f87f6ba4e40a677111
Dohyun Ha <hdh6748@gmail.com>
2024-07-25 14:20:26 +0900

    Add button

--- src/pages/turntable.js
+++ src/pages/turntable.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import TurnTableComponent from '../components/TurnTable';
+import { useRouter } from 'next/router';
+
 const TurnTable = () => {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -23,6 +26,17 @@ const TurnTable = () => {
         overflow: 'hidden'
       }}>
         <TurnTableComponent />
+        <button style={{
+          position: 'absolute', // Use absolute positioning to place the button
+          top: '600px', // Adjust the top position as needed
+          left: '600px', // Adjust the right position as needed
+          padding: '20px 20px',
+          backgroundColor: '#0070f3',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer'
+        }} onClick={() => router.back()}>Go Back</button>
       </main>
     </>
   );
